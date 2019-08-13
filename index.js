1908b2580f41b0dd317f9cb5de4d17767a05a8aa
Nia Laureano <nialaureano@icloud.com>
2019-08-13 02:49:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array;
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
