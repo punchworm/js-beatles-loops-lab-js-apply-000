9721610ea3210c1be7850e185554237b9a1abfcc
Nia Laureano <nialaureano@icloud.com>
2019-08-13 02:53:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,3 +25,20 @@ newArr.unshift(arr[i-1]+"!!!");
 return newArr;
 }
 
+function iLoveTheBeatles(n){
+  var newArr = [];
+  
+  do{
+    newArr.push("I love the Beatles!");
+    n++;
+    console.log(n);
+    // for(var i = n; i >= 0; i--){
+    //     n--;
+    //     newArr.push("I love the Beatles!");
+    //   }
+  }while(n<15);
+  
+  return newArr;
+}
+
+iLoveTheBeatles(55);
